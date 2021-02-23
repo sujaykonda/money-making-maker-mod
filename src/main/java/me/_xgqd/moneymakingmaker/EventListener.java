3ef11f00d37197d3372d6bfb6f2c4a29d6c5c801
sujay <kondasujay2@gmail.com>
2021-02-23 09:31:12 -0500

    version checker fix

--- src/main/java/me/_xgqd/moneymakingmaker/EventListener.java
+++ src/main/java/me/_xgqd/moneymakingmaker/EventListener.java
@@ -1,2 +1,71 @@
-package me._xgqd.moneymakingmaker;public class EventListener {
+package me._xgqd.moneymakingmaker;
+
+import net.minecraft.event.ClickEvent;
+import net.minecraft.event.HoverEvent;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatStyle;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static me._xgqd.moneymakingmaker.Main.VERSION;
+
+public class EventListener {
+
+    private boolean warned = false;
+
+    @SubscribeEvent(priority= EventPriority.NORMAL, receiveCanceled=true)
+    public void onTick(TickEvent.PlayerTickEvent event){
+        if(!warned){
+            System.out.println("Player Logged In");
+            String latestVersion = VERSION;
+            InputStream in = null;
+            try
+            {
+                in = new URL("https://raw.githubusercontent.com/sujaykonda/money-making-maker-mod/main/src/main/resources/version").openStream();
+            }
+            catch
+            (MalformedURLException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            try
+            {
+                latestVersion = IOUtils.readLines(in).get(0);
+                System.out.println(latestVersion);
+            }
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            finally
+            {
+                IOUtils.closeQuietly(in);
+            }
+            System.out.println("Latest mod version = "+latestVersion);
+            if(!VERSION.equals(latestVersion)){
+                ChatComponentText text = new ChatComponentText("MoneyMakingMaker Mod \nNew Update Released \nDownload Link: https://github.com/sujaykonda/money-making-maker-mod/releases \nLatest Version: " + VERSION);
+                ChatStyle style = new ChatStyle();
+                style.setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText("https://github.com/sujaykonda/money-making-maker-mod/releases")));
+                style.setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/sujaykonda/money-making-maker-mod/releases"));
+                text.setChatStyle(style);
+                event.player.addChatMessage(text);
+            }
+            warned = true;
+        }
+    }
 }
--- src/main/java/me/_xgqd/moneymakingmaker/Main.java
+++ src/main/java/me/_xgqd/moneymakingmaker/Main.java
@@ -3,17 +3,39 @@ package me._xgqd.moneymakingmaker;
 import me._xgqd.moneymakingmaker.commands.AuctionFlipCommand;
 import me._xgqd.moneymakingmaker.commands.BinFlipCommand;
 import me._xgqd.moneymakingmaker.hypixel.Hypixel;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.event.ClickEvent;
+import net.minecraft.event.HoverEvent;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatStyle;
 import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ForgeEventFactory;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.EventBus;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @Mod(modid = Main.MODID, version = Main.VERSION, guiFactory = "me._xgqd.moneymakingmaker.ConfigGuiFactory")
 public class Main
 {
     public static final String MODID = "moneymakingmaker";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.1";
     public static Hypixel hypixel;
 
     @EventHandler
@@ -23,6 +45,7 @@ public class Main
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
+        MinecraftForge.EVENT_BUS.register(new EventListener());
         System.out.println("Initializing MoneyMakingMaker");
         ClientCommandHandler.instance.registerCommand(new BinFlipCommand());
         ClientCommandHandler.instance.registerCommand(new AuctionFlipCommand());
